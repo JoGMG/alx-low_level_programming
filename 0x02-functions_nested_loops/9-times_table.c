44fe24f8af82a5bf30a99046ef4613093ebbda1d
JoGMG <joattah3@gmail.com>
2022-10-19 13:44:02 -0700

    9timetable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * times_table -  a function that prints the 9 times table, starting with 0
- * r = row, c = column, d = digits of current result
- * Return: times table
- * add extra space past single digit
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
 int main(void)
-{	
-	char r, c, d;
-
-	for (r = 0; r <= 9; r++)
-	{
-		putchar('0');
-		putchar(',');
-		putchar(' ');
-		for (c = 1; c <= 9; c++)
-		{
-			d = (r * c);
-			if ((d / 10) > 0)
-			{
-				putchar((d / 10) + '0');
-			}
-			else
-			{
-				putchar(' ');
-			}
-				putchar((d % 10) + '0');
-			if (c < 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-		putchar('\n');
-	}
-	int times_table(char *d)
-	{
-	printf("%d", *d);
+{
+	times_table();
 	return (0);
-	}
-}
-
+} 
