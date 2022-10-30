5f5c934a79df4a0f5a45aa821259ac39512510af
JoGMG <joattah3@gmail.com>
2022-10-30 07:32:38 -0700

    betty enabled

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,28 +9,28 @@
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
-    int i;
-    
-    for(i = 1; i <= 100; i++)
-    {
-        if(i % 3 == 0 && i % 5 != 0)
-        {
-            printf("Fizz ");
-        }
-        else if(i % 5 == 0 && i % 3 != 0)
-        {
-            printf("Buzz ");
-        }
-        else if(i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz ");
-        }
-        else
-        {
-            printf("%d ", i);
-        }
-    }
-    return(0);
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
 }
