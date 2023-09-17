41cced2841b7c9cad5cfa8d014deeef131b7cfa6
JoGMG <joattah3@gmail.com>
2023-09-18 00:23:28 +0100

    linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,4 @@
 #include "search_algos.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 /**
  * linear_search - searches for a value in an array of integers
@@ -18,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (!array || size == 0)
+	if (array == NULL)
 		return (-1);
 
 	while (i < size)
