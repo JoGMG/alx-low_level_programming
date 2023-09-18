e7d9bc925ac070eabfb6eb91d35625b51f05679c
JoGMG <joattah3@gmail.com>
2023-09-18 20:27:48 +0100

    singly linked lists

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,16 +1,14 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - Searches for an algorithm in a sorted singly
- *               linked list of integers using linear skip.
- * @list: A pointer to the  head of the linked list to search.
- * @value: The value to search for.
+ * linear_skip - searches for a value in a sorted singly
+ *               linked skip list of integers.
+ * @list: pointer to the head of the skip list to search in.
+ * @value: value to search for.
  *
- * Return: If the value is not present or the head of the list is NULL, NULL.
- *         Otherwise, a pointer to the first node where the value is located.
- *
- * Description: Prints a value every time it is compared in the list.
- *              Uses the square root of the list size as the jump step.
+ * Return: a pointer to the first node where value is located,
+ *         otherwise -1 if the value is not present or the array
+ *         is NULL. 
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
