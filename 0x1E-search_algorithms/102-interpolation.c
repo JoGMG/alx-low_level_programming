fb9c6bfbc789c12b2d323e7f44266b83c3b8eb99
JoGMG <joattah3@gmail.com>
2023-09-18 09:42:09 +0100

    interpolation search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,27 +1,25 @@
 #include "search_algos.h"
 
 /**
-  * interpolation_search - Searches for a value in a sorted array
-  *                        of integers using interpolation search.
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
+  * interpolation_search - searches for a value in a sorted array of
+  *                        integers using interpolation search algorithm.
+  * @array: pointer to the first element of the array to search in.
+  * @size: number of elements in array.
+  * @value: value to search for.
   *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the first index where the value is located.
-  *
-  * Description: Prints a value every time it is compared in the array..
+  * Return: the first index where the value is located, otherwise
+  *         -1 if the value is not present or the array is NULL.
   */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t i, l, r;
+	size_t i, low, high;
 
 	if (array == NULL)
 		return (-1);
 
-	for (l = 0, r = size - 1; r >= l;)
+	for (low = 0, high = size - 1; low <= high;)
 	{
-		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
+		i = low + (value - array[low]) * (((double)(high - low) / (array[high] - array[low])));
 		if (i < size)
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		else
@@ -30,12 +28,12 @@ int interpolation_search(int *array, size_t size, int value)
 			break;
 		}
 
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			r = i - 1;
+		if (array[i] < value)
+			low = i + 1;
+		else if (array[i] > value)
+			high = i - 1;
 		else
-			l = i + 1;
+			return (i);
 	}
 
 	return (-1);
